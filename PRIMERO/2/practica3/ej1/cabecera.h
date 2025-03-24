eb2d09831cdf963166d6df1a060a13be6e817f2c
fernandomartin04 <fernandomartin04@iesamachado.org>
2025-03-25 00:01:04 +0100

    herencias

--- PRIMERO/2/practica3/ej1/cabecera.h
+++ PRIMERO/2/practica3/ej1/cabecera.h
@@ -5,12 +5,28 @@ private:
 	string nombre_empresa;
 public: 
 	// Constructor
-	Paquete(float id, int codigoDestino, string nombre_empresa); 
+	Paquete(int id, int codigoDestino, string nombre_empresa); 
 	
 	// Metodos
 	int getCodigoDestino() const;
 	bool setCodigoDestino(int codigoDestino); 
 	string toString(); 
 	
+};
+class PaqueteComun: public Paquete {
+private:
+	float peso;
+	float precio_por_kilo;
+public:
+	// Constructor
+	PaqueteComun(int id, int codigoDestino, string nombre_empresa, float peso, float precio_por_kilo);
+	
+	// Metodos
+	float getPeso() const;
+	bool setPeso(float peso);
+	float getPrecioPorKilo() const;
+	bool setPrecioPorKilo(float precio_por_kilo);
+	float calcularPrecio();
+	string toString();
 };
 
